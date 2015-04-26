650cf3ecd55334ae54090c23c5b2d73be2947174
Krzysztof Pszczółkowski <pszczolkowski.k@gmail.com>
2015-04-26 11:45:34 +0200

    Dodanie szablonu klasy CommandBuilder

---
+++ src/main/java/commandsFactory/CommandBuilder.java
@@ -0,0 +1,27 @@
+package commandsFactory;
+
+import edu.iis.powp.command.IPlotterCommand;
+
+public class CommandBuilder {
+
+	public CommandBuilder setPosition( int x , int y ){
+		// TODO
+		return this;
+	}
+	
+	public CommandBuilder drawLineTo( int x , int y ){
+		// TODO
+		return this;
+	}
+	
+	public CommandBuilder addCommand( IPlotterCommand command ){
+		// TODO
+		return this;
+	}
+	
+	public CompoundCommand build(){
+		// TODO
+		return null;
+	}
+	
+}
