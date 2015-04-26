71c9f712c7ff474ae503f8dc05a1df666a6b5004
Krzysztof Pszczółkowski <pszczolkowski.k@gmail.com>
2015-04-26 11:42:48 +0200

    Dodanie szablonu klasy CompoundCommand

---
+++ src/main/java/commandsFactory/CompoundCommand.java
@@ -0,0 +1,22 @@
+package commandsFactory;
+
+import java.util.Iterator;
+
+import edu.iis.client.plottermagic.IPlotter;
+import edu.iis.powp.command.ICompoundCommand;
+import edu.iis.powp.command.IPlotterCommand;
+
+public class CompoundCommand implements ICompoundCommand {
+
+	@Override
+	public void execute(IPlotter plotter) {
+		// TODO
+	}
+
+	@Override
+	public Iterator<IPlotterCommand> iterator() {
+		// TODO
+		return null;
+	}
+
+}
