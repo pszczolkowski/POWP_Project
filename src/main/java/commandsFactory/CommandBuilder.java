a4a04627b32a054ae3dd747640e83afca73b3b45
Krzysztof Pszczółkowski <pszczolkowski.k@gmail.com>
2015-04-26 12:27:11 +0200

    Implementacja klasy CommandBuilder

--- src/main/java/commandsFactory/CommandBuilder.java
+++ src/main/java/commandsFactory/CommandBuilder.java
@@ -1,27 +1,33 @@
 package commandsFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.iis.powp.command.DrawToCommand;
 import edu.iis.powp.command.IPlotterCommand;
+import edu.iis.powp.command.SetPositionCommand;
 
 public class CommandBuilder {
+	
+	private List< IPlotterCommand > commands = new ArrayList<>();
 
 	public CommandBuilder setPosition( int x , int y ){
-		// TODO
+		commands.add( new SetPositionCommand( x , y ) );
 		return this;
 	}
 	
 	public CommandBuilder drawLineTo( int x , int y ){
-		// TODO
+		commands.add( new DrawToCommand( x , y ) );
 		return this;
 	}
 	
 	public CommandBuilder addCommand( IPlotterCommand command ){
-		// TODO
+		commands.add( command );
 		return this;
 	}
 	
 	public CompoundCommand build(){
-		// TODO
-		return null;
+		return new CompoundCommand( commands );
 	}
 	
 }
