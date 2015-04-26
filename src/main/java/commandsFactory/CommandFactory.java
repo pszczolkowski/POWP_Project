04db3a3218090566919f78b925a71bb618b20779
Krzysztof Pszczółkowski <pszczolkowski.k@gmail.com>
2015-04-26 11:48:16 +0200

    Dodanie szablonu klasy CommandFactory

---
+++ src/main/java/commandsFactory/CommandFactory.java
@@ -0,0 +1,27 @@
+package commandsFactory;
+
+import java.util.List;
+
+import edu.iis.powp.command.IPlotterCommand;
+
+public class CommandFactory {
+
+	public void add( String name , IPlotterCommand command ){
+		// TODO
+	}
+	
+	public void add( String name , IPlotterCommand command , String category ){
+		// TODO
+	}
+	
+	public IPlotterCommand get( String name ){
+		// TODO
+		return null;
+	}
+	
+	public List< IPlotterCommand > getCommandsOf( String category ){
+		// TODO
+		return null;
+	}
+	
+}
