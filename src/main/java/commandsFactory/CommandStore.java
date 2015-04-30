b8400988f590197a4adc0075720f42bb1016a204
Krzysztof Pszczółkowski <pszczolkowski.k@gmail.com>
2015-04-30 21:49:01 +0200

    Implementacja konstruktora klasy CommandStore

--- src/main/java/commandsFactory/CommandStore.java
+++ src/main/java/commandsFactory/CommandStore.java
@@ -6,13 +6,25 @@ import edu.iis.powp.command.IPlotterCommand;
 
 public class CommandStore {
 
+	private static final String ROOT_CATEGORY_NAME = "all";
+	private static final String DEFAULT_CATEGORY_NAME = "general";
+	
 	private CommandStore instance = null;
+	private CommandCategory defaultCategory;
+	private CommandCategory rootCategory;
 	
 	private CommandStore(){
 		// TODO
 		// odczyt pliku albo oddelegować go na potem
+		
+		// TODO po odczycie z pliku zmodyfkować linię niżej
+		rootCategory = new CommandCategory( ROOT_CATEGORY_NAME );
+		
+		defaultCategory = findCategory( DEFAULT_CATEGORY_NAME );
+		if( defaultCategory == null )
+			defaultCategory = addCategory( DEFAULT_CATEGORY_NAME , null );
 	}
-	
+
 	public CommandStore getInstance(){
 		if( instance == null )
 			instance = new CommandStore();
