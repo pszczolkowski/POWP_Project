05cd18339704a0bce3c180646296ff5b6da26995
Jan Godziatkowski <ph.godzio@gmail.com>
2015-05-02 18:46:19 +0200

    Panel do rysowania komend

---
+++ src/main/java/commandDrawWindow/DrawerPanel.java
@@ -0,0 +1,150 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package commandDrawWindow;
+
+import commandsFactory.CommandBuilder;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Godzio
+ */
+public class DrawerPanel extends JPanel {
+
+    private Point startPoint;
+    private Point endPoint;
+    private boolean draw = false;
+    private boolean preset = true;
+    private final List<Line> lines = new ArrayList<>();
+    private CommandBuilder builder;
+
+    public DrawerPanel() {
+        builder = new CommandBuilder();
+        this.setBackground( Color.white );
+
+        this.addMouseListener(
+                new MouseListener() {
+
+                    @Override
+                    public void mouseClicked( MouseEvent e ) {
+
+                    }
+
+                    @Override
+                    public void mousePressed( MouseEvent e ) {
+                        System.out.println( draw + " " + preset );
+                        if ( !draw || startPoint == null ) {
+                            startPoint = new Point( e.getX(), e.getY() );
+                        } else {
+                            endPoint = new Point( e.getX(), e.getY() );
+                        }
+                        preset = false;
+                        repaint();
+                    }
+
+                    @Override
+                    public void mouseReleased( MouseEvent e ) {
+                    }
+
+                    @Override
+                    public void mouseEntered( MouseEvent e ) {
+                    }
+
+                    @Override
+                    public void mouseExited( MouseEvent e ) {
+                    }
+                } );
+        this.addMouseMotionListener(
+                new MouseMotionListener() {
+
+                    @Override
+                    public void mouseDragged( MouseEvent e ) {
+
+                    }
+
+                    @Override
+                    public void mouseMoved( MouseEvent e ) {
+                        if ( draw ) {
+                            preset = true;
+                            endPoint = new Point( e.getX(), e.getY() );
+                            repaint();
+                        }
+                    }
+                }
+        );
+    }
+
+    @Override
+    public void paint( Graphics g ) {
+        super.paint( g );
+//        RenderingHints rh = new RenderingHints( RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON );
+//        rh.put( RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY );
+//
+//        Graphics2D g = (Graphics2D) g;
+//        g.setRenderingHints( rh );
+        g.setColor( Color.red );
+        if ( startPoint != null ) {
+            if ( draw ) {
+                if ( !preset ) {
+                    lines.add( new Line( startPoint, endPoint ) );
+                    builder.drawLineTo( endPoint.x, endPoint.y );
+                    startPoint = endPoint;
+                } else {
+                    g.drawLine( startPoint.x, startPoint.y, endPoint.x, endPoint.y );
+                }
+                for ( Line line : lines ) {
+                    g.drawLine( line.startPoint.x, line.startPoint.y, line.endPoint.x, line.endPoint.y );
+                }
+
+            } else {
+                endPoint = startPoint;
+                g.drawOval( endPoint.x, endPoint.y, 5, 5 );
+                builder.setPosition( endPoint.x, endPoint.y );
+                for ( Line line : lines ) {
+                    g.drawLine( line.startPoint.x, line.startPoint.y, line.endPoint.x, line.endPoint.y );
+                }
+            }
+        }
+        preset = true;
+    }
+
+    public void setDraw( boolean draw ) {
+        this.draw = draw;
+    }
+
+    public CommandBuilder getBuilder() {
+        return builder;
+    }
+
+    public void newBuilder() {
+        this.builder = new CommandBuilder();
+    }
+
+    public void clear() {
+        lines.clear();
+        newBuilder();
+        repaint();
+    }
+
+    void setPosition( int x, int y ) {
+        startPoint = new Point( x, y );
+        repaint();
+    }
+
+    void drawLine( int x, int y ) {
+        endPoint = new Point( x, y );
+        repaint();
+    }
+
+}
